4b4b9de62d8ec2bea10b0f5a9e7004c46126497b
Kazi Ashik <kaziashik999@gmail.com>
2022-09-18 10:40:09 +0600

    create decorator for token decode

--- src/@domain/extensions/getcurrentadminuser.decorator.ts
+++ src/@domain/extensions/getcurrentadminuser.decorator.ts
@@ -10,6 +10,7 @@ export const GetCurrentAdminUser = createParamDecorator(
   (data: IJwtPayload, ctx: ExecutionContext) => {
     console.log(data, ctx)
     const request = ctx.switchToHttp().getRequest();
+    // request.user.
     console.log(data, request)
     return request.user;
   },
